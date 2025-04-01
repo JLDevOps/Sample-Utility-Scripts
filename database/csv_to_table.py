4093d6eacf4cde42c3b55ad9f37c181e96ada021
Jim Le <jldevops@gmail.com>
2025-03-31 21:04:21 -0400

    Updated

--- database/csv_to_table.py
+++ database/csv_to_table.py
@@ -1,4 +1,4 @@
-import csv
+import pandas as pd
 import argparse
 
 # CSV to Database Table Mapping
@@ -8,14 +8,25 @@ column_mapping = {
     'csv_col5': 'db_colC'
 }
 
-def generate_sql(csv_file, output_sql):
-    with open(csv_file, newline='', encoding='utf-8') as f, open(output_sql, 'w', encoding='utf-8') as out_f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            values = [row[c].replace("'", "''") for c in column_mapping.keys()]
-            columns_sql = ', '.join(column_mapping.values())
-            values_sql = ', '.join(f"'{v}'" for v in values)
-            sql = f"INSERT INTO my_table ({columns_sql}) VALUES ({values_sql});"
+def generate_sql(csv_file, output_sql, table):
+    df = pd.read_csv(csv_file)
+    df = df[list(column_mapping.keys())]
+    df.rename(columns=column_mapping, inplace=True)
+
+    with open(output_sql, 'w', encoding='utf-8') as out_f:
+        for _, row in df.iterrows():
+            values = []
+            for value in row:
+                if pd.isnull(value):
+                    values.append('NULL')
+                elif isinstance(value, (int, float)):
+                    values.append(str(value))
+                else:
+                    values.append("'" + str(value).replace("'", "''") + "'")
+
+            columns_sql = ', '.join(df.columns)
+            values_sql = ', '.join(values)
+            sql = f"INSERT INTO {table} ({columns_sql}) VALUES ({values_sql});"
             out_f.write(sql + '\n')
     print(f"SQL statements written to {output_sql}")
 
@@ -23,8 +34,10 @@ def main():
     parser = argparse.ArgumentParser(description="Generate SQL INSERT statements from CSV")
     parser.add_argument('--csv', type=str, default='data.csv', help="Path to the input CSV file")
     parser.add_argument('--output', type=str, default='insert_statements.sql', help="Path to the output SQL file")
+    parser.add_argument('--table', type=str, default='table', help="Table Name")
+
     args = parser.parse_args()
-    generate_sql(args.csv, args.output)
+    generate_sql(args.csv, args.output, args.table)
 
 if __name__=="__main__":
     main()
