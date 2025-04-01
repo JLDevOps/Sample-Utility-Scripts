b8eff0da0040d546b1bc9eda49b86e288075936c
Jim Le <jldevops@gmail.com>
2025-03-31 22:44:13 -0400

    updated

--- duration_analysis.py
+++ duration_analysis.py
@@ -55,3 +55,53 @@ df['MET_RTO'] = df.apply(check_rto, axis=1)
 df.to_csv(output_csv, index=False)
 
 print(f"Result in {output_csv}")
+
+
+WITH cte AS (
+    SELECT 
+        app_id,
+        rto,
+        start_time_utc,
+        end_time_utc,
+        -- Convert '2025-03-21 20:00:00' CT to UTC
+        DATEADD(hour, DATEDIFF(hour, GETDATE(), GETUTCDATE()), '2025-03-21 20:00:00') AS fixed_start_utc
+    FROM your_table
+)
+SELECT 
+    app_id,
+    rto,
+    start_time_utc,
+    end_time_utc,
+    -- Calculate duration in hours
+    DATEDIFF(SECOND, cte.fixed_start_utc, cte.end_time_utc) / 3600.0 AS duration_hours,
+
+    -- Determine MET_RTO
+    CASE 
+        WHEN rto = 0 AND DATEDIFF(SECOND, cte.fixed_start_utc, cte.end_time_utc) / 3600.0 BETWEEN 1 AND 2 THEN 1
+        WHEN rto = 1 AND DATEDIFF(SECOND, cte.fixed_start_utc, cte.end_time_utc) / 3600.0 BETWEEN 4 AND 6 THEN 1
+        WHEN rto = 2 AND DATEDIFF(SECOND, cte.fixed_start_utc, cte.end_time_utc) / 3600.0 BETWEEN 8 AND 10 THEN 1
+        ELSE 0
+    END AS MET_RTO
+FROM cte;
+
+-- Example: rto_table(rto, min_hours, max_hours)
+
+WITH cte AS (
+    SELECT 
+        app_id,
+        yt.rto,
+        start_time_utc,
+        end_time_utc,
+        DATEADD(hour, DATEDIFF(hour, GETDATE(), GETUTCDATE()), '2025-03-21 20:00:00') AS fixed_start_utc
+    FROM your_table yt
+)
+SELECT 
+    cte.app_id,
+    cte.rto,
+    cte.start_time_utc,
+    cte.end_time_utc,
+    DATEDIFF(SECOND, cte.fixed_start_utc, cte.end_time_utc) / 3600.0 AS duration_hours,
+    CASE WHEN DATEDIFF(SECOND, cte.fixed_start_utc, cte.end_time_utc) / 3600.0 BETWEEN rt.min_hours AND rt.max_hours THEN 1 ELSE 0 END AS MET_RTO
+FROM cte
+LEFT JOIN rto_table rt
+    ON cte.rto = rt.rto;
